0a05a3775b26148b3dca691e4b09dd4949aa185d
Nicole <z5677783@ad.unsw.edu.au>
2026-02-14 13:10:56 +1100

    Implement backend task 2: add cookbook entry

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+const cookbook: (recipe | ingredient)[] = [];
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -62,10 +62,49 @@ const parse_handwriting = (recipeName: string): string | null => {
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const { type, name, requiredItems, cookTime } = req.body;
 
+  try {
+    res.json(addEntry(type, name, requiredItems, cookTime));
+  } catch(err) {
+    console.log(err.message);
+    res.status(400).json({});
+  }
 });
+
+const addEntry = (type: string, name: string, requiredItems: requiredItem[] = [],
+                  cookTime: number = -1) => {
+  if (type !== "recipe" && type !== "ingredient") {
+    throw new Error("type can only be \"recipe\" or \"ingredient\".");
+  }
+
+  if (cookbook.some(e => e.name === name)) {
+    throw new Error("entry names must be unique");
+  }
+
+  let entry: recipe | ingredient;
+
+  if (type === "ingredient") {
+    if (cookTime < 0) {
+      throw new Error("cookTime can only be greater than or equal to 0");
+    }
+
+    entry = { type, name, cookTime };
+  } else {
+    // type === 'recipe'
+    // check unique element names
+    const elements = new Set();
+    requiredItems.forEach(e => elements.add(e.name.toLowerCase()));
+    if (elements.size !== requiredItems.length) {
+      throw new Error("Recipe requiredItems can only have one element per name.");
+    }
+
+    entry = { type, name, requiredItems };
+  }
+
+  cookbook.push(entry);
+  return {};
+}
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
