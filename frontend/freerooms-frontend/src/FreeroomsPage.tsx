d93062771e2003a1a96b99c8ee260c7078d7eef6
Nicole <z5677783@ad.unsw.edu.au>
2026-02-20 10:18:16 +1100

    Delete header file and move layout to Freerooms Page.

--- frontend/freerooms-frontend/src/FreeroomsPage.tsx
+++ frontend/freerooms-frontend/src/FreeroomsPage.tsx
@@ -1,12 +1,36 @@
-import { CardGrid } from "./components/CardGrid";
-import Header from "./components/Header";
+import FilterAltIcon from "@mui/icons-material/FilterAlt"
+import FilterListIcon from "@mui/icons-material/FilterList"
+
+import FilterButton from "./components/FilterButton";
+import SearchBar from "./components/SearchBar";
+import Logo from "./components/Logo";
+import IconGroup from "./components/IconGroup";
+import CardGrid from "./components/CardGrid";
 
 export default function FreeroomsPage() {
     return (
         <div className="bg-white min-h-screen">
-            <Header />
-            <div className="mx-auto max-w-full p-4">
-                <CardGrid />
+            <div className="p-4 mx-auto">
+                {/* Top bar */}
+                <div className="sticky bg-white top-0 z-10 flex items-center justify-between border-b border-gray-300 p-2">
+                    <Logo />
+                    <IconGroup />
+                </div>
+                {/* Filter, search and sort */}
+                <div className="flex items-center justify-between m-3 gap-8">
+                    <FilterButton label="Filter">
+                        <FilterAltIcon />
+                    </FilterButton>
+
+                    <SearchBar />
+
+                    <FilterButton label="Sort">
+                        <FilterListIcon />
+                    </FilterButton>
+                </div>
+                <div className="max-w-full">
+                    <CardGrid />
+                </div>
             </div>
         </div>
     );
--- frontend/freerooms-frontend/src/components/CardGrid.tsx
+++ frontend/freerooms-frontend/src/components/CardGrid.tsx
@@ -1,7 +1,7 @@
 import buildings from "../data.json"
 import { BuildingCard } from "./BuildingCard"
 
-export const CardGrid = () => {
+export default function CardGrid() {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             {buildings.map(b => (
